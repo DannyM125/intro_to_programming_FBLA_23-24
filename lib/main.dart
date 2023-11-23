b2b7adea9e6db5ec2fd3c9d56189698612045800
Daniel Mahany <daniel.mahany@gmail.com>
2023-11-22 19:02:50 -0500

    NEW LAYOUT IDEA + New colors + icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GPA Calculator',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Page 1'),
+      home: CalculatorScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class CalculatorScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CalculatorScreenState createState() => _CalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String selectedClass = 'class1'; // Default value
-
-  TextEditingController gradeController = TextEditingController();
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  List<String> grades = ['A', 'B', 'C', 'D', 'F'];
+  String selectedGrade = 'A';
+  List<double> credits = [4.0, 2.5];
+  double selectedCredits = 4.0;
+  List<Map<String, dynamic>> courses = [];
+  TextEditingController courseNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('GPA Calculator'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(height: 10),
             Text(
-              'Choose Class & Grade:',
+              'Add Course:',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 5),
+            SizedBox(height: 8),
             Row(
               children: [
+                Flexible(
+                  child: TextField(
+                    controller: courseNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Course Name',
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16),
                 DropdownButton<String>(
-                  value: selectedClass,
+                  value: selectedGrade,
                   onChanged: (String? newValue) {
                     setState(() {
-                      selectedClass = newValue!;
+                      selectedGrade = newValue!;
                     });
                   },
-                  items: <String>['Choose class', 'class1', 'class2']
+                  items: grades
                       .map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
                       value: value,
@@ -69,17 +76,65 @@ class _MyHomePageState extends State<MyHomePage> {
                   }).toList(),
                 ),
                 SizedBox(width: 16),
-                SizedBox(width: 8),
-                Container(
-                  width: 100, // Adjust the width as needed
-                  child: TextField(
-                    controller: gradeController,
-                    maxLength: 1,
-                    decoration: InputDecoration(
-                      hintText: 'Letter Grade',
-                    ),
-                  ),
+                DropdownButton<double>(
+                  value: selectedCredits,
+                  onChanged: (double? newValue) {
+                    setState(() {
+                      selectedCredits = newValue!;
+                    });
+                  },
+                  items: credits
+                      .map<DropdownMenuItem<double>>((double value) {
+                    return DropdownMenuItem<double>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
                 ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    addCourse();
+                  },
+                  child: Text('Add'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Courses:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: ListView.builder(
+                itemCount: courses.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(courses[index]['name']),
+                    subtitle: Text(
+                      'Grade: ${courses[index]['grade']}, Credit Hours: ${courses[index]['credits']}',
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Colors.red,
+                      onPressed: () {
+                        removeCourse(index);
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 20),
+                Text(
+                  'GPA: ${calculateGPA().toStringAsFixed(2)}',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+            SizedBox(height: 40),
               ],
             ),
           ],
@@ -87,4 +142,58 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  void addCourse() {
+    String courseName = courseNameController.text;
+    if (courseName.isNotEmpty) {
+      Map<String, dynamic> course = {
+        'name': courseName,
+        'grade': selectedGrade,
+        'credits': selectedCredits,
+      };
+      setState(() {
+        courses.add(course);
+        courseNameController.text = '';
+      });
+    }
+  }
+
+  void removeCourse(int index) {
+    setState(() {
+      courses.removeAt(index);
+    });
+  }
+
+  double calculateGPA() {
+    double totalPoints = 0;
+    double totalCredits = 0;
+
+    for (var course in courses) {
+      totalPoints += gradeToPoint(course['grade']) * course['credits'];
+      totalCredits += course['credits'];
+    }
+
+    return totalPoints / totalCredits;
+  }
+
+  double gradeToPoint(String grade) {
+    switch (grade) {
+      case 'A':
+        return 4.0;
+      case 'B':
+        return 3.0;
+      case 'C':
+        return 2.0;
+      case 'D':
+        return 1.0;
+      default:
+        return 0.0;
+    }
+  }
 }
+/* 
+TODO: FIX GPA CALCULATIONS ACORDING TO HOW OUR SCHOOL DOES IT!!!
+
+
+
+ */
