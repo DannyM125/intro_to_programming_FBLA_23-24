0dc7877271aed21cee346e2c673ae832f6caf6f5
Megh-P <meghp270@gmail.com>
2023-11-24 18:20:26 -0500

    impemented saving (have to do it by coursesByGrade)

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,8 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:intro_to_programming_fbla/Course.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'stats.dart';
-
-class Course {
-  String name;
-  String grade;
-  double credits;
-
-  Course({
-    required this.name,
-    required this.grade,
-    required this.credits,
-  });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'grade': grade,
-      'credits': credits,
-    };
-  }
-
-  factory Course.fromJson(Map<String, dynamic> json) {
-    return Course(
-      name: json['name'],
-      grade: json['grade'],
-      credits: json['credits'],
-    );
-  }
-}
 
 void main() {
   runApp(MyApp());
