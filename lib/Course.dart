3d670dbad7644f9d332d7371dd2d433bed5a64f0
Megh-P <meghp270@gmail.com>
2023-11-25 11:18:51 -0500

    saving the courses works (finally!!!)

--- lib/Course.dart
+++ lib/Course.dart
@@ -25,3 +25,54 @@ class Course {
     );
   }
 }
+
+class Course_9 extends Course {
+  Course_9({required super.name, required super.grade, required super.credits});
+
+  factory Course_9.fromJson(Map<String, dynamic> json) {
+    return Course_9(
+      name: json['name'],
+      grade: json['grade'],
+      credits: json['credits'],
+    );
+  }
+}
+
+class Course_10 extends Course {
+  Course_10(
+      {required super.name, required super.grade, required super.credits});
+
+  factory Course_10.fromJson(Map<String, dynamic> json) {
+    return Course_10(
+      name: json['name'],
+      grade: json['grade'],
+      credits: json['credits'],
+    );
+  }
+}
+
+class Course_11 extends Course {
+  Course_11(
+      {required super.name, required super.grade, required super.credits});
+
+  factory Course_11.fromJson(Map<String, dynamic> json) {
+    return Course_11(
+      name: json['name'],
+      grade: json['grade'],
+      credits: json['credits'],
+    );
+  }
+}
+
+class Course_12 extends Course {
+  Course_12(
+      {required super.name, required super.grade, required super.credits});
+
+  factory Course_12.fromJson(Map<String, dynamic> json) {
+    return Course_12(
+      name: json['name'],
+      grade: json['grade'],
+      credits: json['credits'],
+    );
+  }
+}
