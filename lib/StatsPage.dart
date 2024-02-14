3b5feb29c9caa49c0dfaeb4a97cd10fda4af7350
Megh-P <meghp270@gmail.com>
2024-02-13 19:13:02 -0500

    Added WidgetUtils

--- lib/StatsPage.dart
+++ lib/StatsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intro_to_programming_fbla/CoursePage.dart';
+import 'package:intro_to_programming_fbla/WidgetUtils.dart';
 
 class StatisticsScreen extends StatelessWidget {
   @override
@@ -13,7 +14,6 @@ class StatisticsScreen extends StatelessWidget {
             fontWeight: FontWeight.bold, // Font weight (bold)
             fontStyle: FontStyle.italic, // Font style (italic)
             color: Colors.white, // Text color
-            
           ),
         ),
       ),
@@ -26,113 +26,10 @@ class StatisticsScreen extends StatelessWidget {
             RoundedWeightedGPABox(),
             SizedBox(height: 20), // Spacer between GPA boxes and button
             editCoursesButton(),
-            SizedBox(height: 200,)
+            SizedBox(
+              height: 200,
+            )
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedGPABox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 150,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(60),
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            '???',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 50,
-            ),
-          ),
-          SizedBox(height: 10), // Spacer between "???" and "Weighted GPA"
-          Text(
-            'Unweighted GPA',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RoundedWeightedGPABox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 150,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(60),
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            '???',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 50,
-            ),
-          ),
-          SizedBox(height: 10), // Spacer between "???" and "Weighted GPA"
-          Text(
-            'Weighted GPA',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class editCoursesButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => CalculatorScreen()),
-        );
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blue, // Background color
-        foregroundColor: Colors.white, // Text color
-        padding: EdgeInsets.symmetric(
-            vertical: 15, horizontal: 50), // Button padding
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20), // Button border radius
-        ),
-      ),
-      child: Text(
-        'Edit Courses',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
         ),
       ),
     );
---
+++ lib/WidgetUtils.dart
@@ -0,0 +1,109 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:intro_to_programming_fbla/CoursePage.dart';
+
+class RoundedGPABox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 150,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(60),
+      ),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '???',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+            ),
+          ),
+          SizedBox(height: 10), // Spacer between "???" and "Weighted GPA"
+          Text(
+            'Unweighted GPA',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RoundedWeightedGPABox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 150,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(60),
+      ),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '???',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+            ),
+          ),
+          SizedBox(height: 10), // Spacer between "???" and "Weighted GPA"
+          Text(
+            'Weighted GPA',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class editCoursesButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => CalculatorScreen()),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue, // Background color
+        foregroundColor: Colors.white, // Text color
+        padding: EdgeInsets.symmetric(
+            vertical: 15, horizontal: 50), // Button padding
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20), // Button border radius
+        ),
+      ),
+      child: Text(
+        'Edit Courses',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
