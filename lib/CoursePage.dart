558e0a0420513bb98b6de7b2eaf95df5b79603e3
Daniel Mahany <daniel.mahany@gmail.com>
2024-03-02 19:26:58 -0500

    Formatting changes, added Edit button, Changes to how we save courses to allow for changing a courses grade level.

--- lib/CoursePage.dart
+++ lib/CoursePage.dart
@@ -34,10 +34,18 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GPA Calculator'),
+        title: Text(
+          'Update Courses',
+          style: GoogleFonts.poppins(
+              fontSize: 25, // Adjust font size as needed
+              fontWeight: FontWeight.w600,
+              color: Colors.white),
+        ),
         leading: IconButton(
           icon:
-              Icon(Icons.arrow_back), // Use arrow_back icon for back navigation
+              Icon(Icons.arrow_back,
+              size: 35,
+              ), // Use arrow_back icon for back navigation
           onPressed: () {
             Navigator.pop(context); // Go back to the previous screen
           },
@@ -95,7 +103,9 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                   ),
                 ],
               ),
+              SizedBox(height: 50)
             ],
+            
           ),
         ),
       ),
@@ -108,7 +118,15 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     );
   }
 
+//Dialog methods:
+
   void _showAddCourseDialog() {
+    // Set initial values for the dialog fields
+    courseNameController.text = '';
+    selectedGradeLevel = gradeLevels.first;
+    selectedLetterGrade = letterGrades.first;
+    selectedCredits = credits.first;
+
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -220,12 +238,13 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('Cancel',
-              style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                    ),
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                ),
               ),
             ),
             ElevatedButton(
@@ -233,12 +252,14 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                 addCourse();
                 Navigator.of(context).pop();
               },
-              child: Text('Add',
-              style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                    ),),
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),
+              ),
             ),
           ],
         );
@@ -246,58 +267,158 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     );
   }
 
-  Widget printCourses() {
-    List<Widget> courseWidgets = [];
-    for (int gradeLevel in gradeLevels) {
-      courseWidgets.add(
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Courses for Grade $gradeLevel:',
-              style: GoogleFonts.poppins(
-                fontSize: 23,
-                fontWeight: FontWeight.w600,
-                color: Colors.blueGrey,
+  void _showEditCourseDialog(
+    dynamic course,
+    int gradeLevel,
+    int index,
+  ) {
+    // Set initial values for the dialog fields
+    courseNameController.text = course.name;
+    selectedGradeLevel = gradeLevel;
+    selectedLetterGrade = course.grade;
+    selectedCredits = course.credits;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Course'),
+          content: Container(
+            width: 320,
+            height: 150,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: courseNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Course Name',
+                    labelStyle: TextStyle(fontSize: 25),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: [
+                    StatefulBuilder(
+                      builder: (BuildContext context, StateSetter setState) {
+                        return DropdownButton<int>(
+                          value: selectedGradeLevel,
+                          onChanged: (int? newValue) {
+                            setState(() {
+                              selectedGradeLevel = newValue!;
+                            });
+                          },
+                          items: gradeLevels
+                              .map<DropdownMenuItem<int>>((int value) {
+                            return DropdownMenuItem<int>(
+                              value: value,
+                              child: Text(
+                                'Grade $value',
+                                style: TextStyle(
+                                  fontSize: 20, // Font size
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    StatefulBuilder(
+                      builder: (BuildContext context, StateSetter setState) {
+                        return DropdownButton<String>(
+                          value: selectedLetterGrade,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedLetterGrade = newValue!;
+                            });
+                          },
+                          items: letterGrades
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(
+                                  fontSize: 20, // Font size
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    StatefulBuilder(
+                      builder: (BuildContext context, StateSetter setState) {
+                        return DropdownButton<double>(
+                          value: selectedCredits,
+                          onChanged: (double? newValue) {
+                            setState(() {
+                              selectedCredits = newValue!;
+                            });
+                          },
+                          items: credits
+                              .map<DropdownMenuItem<double>>((double value) {
+                            return DropdownMenuItem<double>(
+                              value: value,
+                              child: Text(
+                                value.toString(),
+                                style: TextStyle(
+                                  fontSize: 20, // Font size
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                ),
               ),
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: getCourseList(gradeLevel).length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(getCourseList(gradeLevel)[index].name,
-                      style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
-                    ),),
-                  subtitle: Text(
-                    'Grade: ${getCourseList(gradeLevel)[index].grade}, Credits: ${getCourseList(gradeLevel)[index].credits}',
-                    style: GoogleFonts.poppins(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black54,
-                    ),
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Colors.red,
-                    onPressed: () {
-                      removeCourse(gradeLevel, index);
-                    },
-                  ),
-                );
+            ElevatedButton(
+              onPressed: () {
+                updateCourse(gradeLevel, index);
+                Navigator.of(context).pop();
               },
+              child: Text(
+                'Update',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),
+              ),
             ),
           ],
-        ),
-      );
-    }
-    return Column(children: courseWidgets);
+        );
+      },
+    );
   }
 
+//Edit Course list methods:
   void addCourse() {
     String courseName = courseNameController.text;
     if (selectedGradeLevel == 9) {
@@ -377,6 +498,68 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     });
   }
 
+void updateCourse(int oldGradeLevel, int index) {
+  String courseName = courseNameController.text;
+  if (courseName.isNotEmpty) {
+    setState(() {
+      switch (oldGradeLevel) {
+        case 9:
+          var course = courses_9.removeAt(index);
+          break;
+        case 10:
+          var course = courses_10.removeAt(index);
+          break;
+        case 11:
+          var course = courses_11.removeAt(index);
+          break;
+        case 12:
+          var course = courses_12.removeAt(index);
+          break;
+        default:
+          break;
+      }
+
+      switch (selectedGradeLevel) {
+        case 9:
+          courses_9.add(Course_9(
+            name: courseName,
+            grade: selectedLetterGrade,
+            credits: selectedCredits,
+          ));
+          break;
+        case 10:
+          courses_10.add(Course_10(
+            name: courseName,
+            grade: selectedLetterGrade,
+            credits: selectedCredits,
+          ));
+          break;
+        case 11:
+          courses_11.add(Course_11(
+            name: courseName,
+            grade: selectedLetterGrade,
+            credits: selectedCredits,
+          ));
+          break;
+        case 12:
+          courses_12.add(Course_12(
+            name: courseName,
+            grade: selectedLetterGrade,
+            credits: selectedCredits,
+          ));
+          break;
+        default:
+          break;
+      }
+    });
+    saveCourses();
+  }
+}
+
+
+
+
+
   void saveCourses() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String> coursesJson =
@@ -440,6 +623,97 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     }
   }
 
+  Widget printCourses() {
+    List<Widget> courseWidgets = [];
+    for (int gradeLevel in gradeLevels) {
+      courseWidgets.add(
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Courses for Grade $gradeLevel:',
+              style: GoogleFonts.poppins(
+                fontSize: 23,
+                fontWeight: FontWeight.w600,
+                color: Colors.blueGrey,
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: getCourseList(gradeLevel).length,
+              itemBuilder: (context, index) {
+                final course = getCourseList(gradeLevel)[index];
+                return ListTile(
+                  title: Text(
+                    course.name,
+                    style: GoogleFonts.poppins(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Grade: ${course.grade}, Credits: ${course.credits}',
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.edit,
+                          size: 30.0,
+                        ),
+                        color: Colors.blue,
+                        onPressed: () {
+                          _showEditCourseDialog(course, gradeLevel, index);
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.delete,
+                          size: 30.0,
+                        ),
+                        color: Colors.red,
+                        onPressed: () {
+                          removeCourse(gradeLevel, index);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      );
+    }
+    return Column(children: courseWidgets);
+  }
+
+  List<dynamic> getCourseList(int selectedGradeLevel) {
+    switch (selectedGradeLevel) {
+      case 9:
+        return courses_9;
+      case 10:
+        return courses_10;
+      case 11:
+        return courses_11;
+      case 12:
+        return courses_12;
+      default:
+        return courses_9;
+    }
+  }
+
+//GPA calculation methods:
+  double get weightedGPA => calculateWeightedGPA();
+
   double calculateGPA() {
     int count = 0;
     double totalPoints = 0;
@@ -456,17 +730,6 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     }
     return totalCredits != 0 ? totalPoints / totalCredits : 0.0;
   }
-/*   double calculateGPA(int gradeLevel) {
-    double totalPoints = 0;
-    double totalCredits = 0;
-
-    for (var course in getCourseList(gradeLevel)) {
-      totalPoints += gradeToPointUW(course.grade) * course.credits;
-      totalCredits += course.credits;
-    }
-
-    return totalCredits != 0 ? totalPoints / totalCredits : 0.0;
-  } */
 
   //TODO this is identical to UW currently
   //TODO CHANGE THIS TO ACCOUNT FOR DE and AP
@@ -487,8 +750,6 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     return totalCredits != 0 ? totalPoints / totalCredits : 0.0;
   }
 
-  double get weightedGPA => calculateWeightedGPA();
-
   double gradeToPointUW(String grade) {
     switch (grade) {
       case 'A':
@@ -505,21 +766,6 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
         return 1.0;
       default:
         return 0.0;
-    }
-  }
-
-  List<dynamic> getCourseList(int selectedGradeLevel) {
-    switch (selectedGradeLevel) {
-      case 9:
-        return courses_9;
-      case 10:
-        return courses_10;
-      case 11:
-        return courses_11;
-      case 12:
-        return courses_12;
-      default:
-        return courses_9;
     }
   }
 }
